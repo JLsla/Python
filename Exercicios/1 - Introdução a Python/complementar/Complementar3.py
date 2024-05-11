e6864f5db14cf7c06b52fe245c28c1d4822713e4
JLsla <168488850+JLsla@users.noreply.github.com>
2024-05-11 06:17:31 -0300

    Complementar3.py

--- Exercicios/1 - Introdução a Python/complementar/Complementar3.py
+++ Exercicios/1 - Introdução a Python/complementar/Complementar3.py
@@ -1,1 +1,5 @@
 # 3. Escreva um programa que leia duas variáveis inteiras e troque o conteúdo entre elas, mostrando o resultado.
+
+nome = str(input("Qual o seu nome? "))
+sobrenome = str(input("Qual o seu sobrenome? "))
+print(f"O seu nome e sobrenome ao contrário fica {sobrenome} {nome}")
